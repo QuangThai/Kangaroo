a809e985b7ca54922b21fa2d9e5aab9488046205
QuangThai <nguyenquangthaib1@gmail.com>
2019-09-29 22:32:50 +0700

    Thay đổi 1 số cái cho đẹp code + tooltip bên trái

--- js/main.js
+++ js/main.js
@@ -1,52 +1,60 @@
-$(document).ready(function() {
-		// sticky
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 200){  
-				$('#header-mid').addClass("sticky");
-			}
-			else{
-				$('#header-mid').removeClass("sticky");
-			}
-		});
+$(document).ready(function () {
+    // sticky
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#header-mid').addClass("sticky");
+        } else {
+            $('#header-mid').removeClass("sticky");
+        }
+    });
 
-		//ScrollUp
-		$.scrollUp({
-			scrollText: '<i class="fas fa-chevron-up"></i>',
-			easingType: 'linear',
-			scrollSpeed: 600,
-			animation: 'fade'
-		});
+    //ScrollUp
+    $.scrollUp({
+        scrollText: '<i class="fas fa-chevron-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 600,
+        animation: 'fade'
+    });
 
-		// Carousel
-		$('.owl-carousel').owlCarousel({
-			loop:true,
-			autoplay:true,
-			margin:2,
-			nav:false,
-			responsive:{
-				0:{ items:2 },
-				480:{ items:4 },
-				768:{ items:6 },
-				992:{ items:6 },
-				1200:{ items:8 }
-			}
-		});
+    // Carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 2,
+        nav: false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            480: {
+                items: 4
+            },
+            768: {
+                items: 6
+            },
+            992: {
+                items: 6
+            },
+            1200: {
+                items: 8
+            }
+        }
+    });
 
-		// Tooltip
-		$(function () {
-			$('[data-hieuung="tooltip"]').tooltip();
-		});
+    // Tooltip
+    $(function () {
+        $('[data-hieuung="tooltip"]').tooltip();
+    });
 
-		//Scroll Menu
-		$(window).scroll(function() {
-			var height = $(window).scrollTop();
-			if (height >= 500){ 
-				$('.scroll-menu, .scroll-qc').addClass('visible');
-			}
-			else {
-				$('.scroll-menu, .scroll-qc').removeClass('visible');
-			}
-		});
+    //Scroll Menu
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+        if (height >= 500) {
+            $('.scroll-menu, .scroll-qc').addClass('visible');
+        } else {
+            $('.scroll-menu, .scroll-qc').removeClass('visible');
+        }
+    });
 
 });
 
